7764c604807664f44d970d45ebc9a5e5338f677a
KaleB-CS30 <43385287+KaleBerger@users.noreply.github.com>
2018-11-30 11:05:24 -0600

    Sprite files and settings for the sprites
    Files contain the player and wall sprites for the main code. future versions will include improved player sprite and mob sprites.

---
+++ settings.py
@@ -0,0 +1,29 @@
+#################################################
+#CS30 Final Capstone Project
+#Developers: Kale, Dallas, Marco
+#
+#Settings
+#################################################
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+#Game Settings
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+TITLE = "Top-Down Shooter"
+BGCOLOR = DARKGREY
+
+TILESIZE = 64
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+# Player settings
+PLAYER_SPEED = 300.0
+

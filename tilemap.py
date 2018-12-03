deb7e3ce0c53b9ad22053a483da47ded8d78c4e8
DallasSCS30 <43384932+DallasSCS30@users.noreply.github.com>
2018-12-03 11:35:39 -0600

    Fixed minor issue with empty space

--- tilemap.py
+++ tilemap.py
@@ -34,7 +34,7 @@ class Map:
         self.data = []
         with open(filename, 'rt') as f:
             for line in f:
-                self.data.append(line)
+                self.data.append(line.strip())
     
         self.tileWidth = len(self.data[0])
         self.tileHeight = len(self.data)

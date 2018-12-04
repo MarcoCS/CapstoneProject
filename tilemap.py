5b09ba50890380607cc0177e433a0a6817249f95
KaleB-CS30 <43385287+KaleBerger@users.noreply.github.com>
2018-12-04 11:14:51 -0600

    camera now follows center of sprite

--- tilemap.py
+++ tilemap.py
@@ -18,8 +18,8 @@ class Camera:
         return entity.rect.move(self.camera.topleft)
     
     def update(self, target):
-        x = -target.rect.x + int(WIDTH / 2)
-        y = -target.rect.y + int(HEIGHT / 2)
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
         
         ## Limit scrolling to map
         x = min(0, x) ## Left

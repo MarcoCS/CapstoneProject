7b48c7dca0f3a6e6e6fae3c839ab62ea803f353e
DallasSCS30 <43384932+DallasSCS30@users.noreply.github.com>
2018-12-06 11:19:20 -0600

    Commented

--- tilemap.py
+++ tilemap.py
@@ -18,13 +18,14 @@ class Camera:
         self.height = height
         
     def apply(self, entity):
+        # Each entity is offset and this creates scrolling. 
         return entity.rect.move(self.camera.topleft)
     
     def update(self, target):
         x = -target.rect.centerx + int(WIDTH / 2)
         y = -target.rect.centery + int(HEIGHT / 2)
         
-        ## Limit scrolling to map
+        # Limit scrolling to map
         x = min(0, x) ## Left
         y = min(0, y) ## Top
         x = max(-(self.width - WIDTH), x)   ## Right
@@ -33,6 +34,7 @@ class Camera:
         self.camera = pg.Rect(x, y, self.width, self.height)
         
 class Map:
+    # Let the map be its own object. 
     def __init__(self, filename):
         self.data = []
         with open(filename, 'rt') as f:

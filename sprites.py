92528cfc483ca63fb583d38e8a5d6367dd532e20
KaleB-CS30 <43385287+KaleBerger@users.noreply.github.com>
2018-12-17 11:40:27 -0600

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -109,7 +109,7 @@ class Bullet(pg.sprite.Sprite):
 class Healthpower(pg.sprite.Sprite):
     def __init__(self, center):
         pg.sprite.Sprite.__init__(self)
-        self.type = choice([normalheal, superheal])
+        self.type = choice(['normalheal', 'superheal'])
         self.image = healthupImages[self.type]
         self.rect = self.image.get_rect()
         self.rect.center = center

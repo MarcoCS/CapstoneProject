ee7f5d570b4bc575d055345210bf585e13ec8d97
KaleB-CS30 <43385287+KaleBerger@users.noreply.github.com>
2018-12-17 11:52:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ class Game:
         self.bullet_img = pg.image.load(path.join(img_folder, BULLET_IMG)).convert_alpha()
         self.mobImage = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
         self.shooterImage = pg.image.load(path.join(img_folder, SHOOTER_IMG)).convert_alpha()
-        healthupImages = {}
-        healthupImages['normalheal'] = pg.image.load(path.join(img_folder, HEALTHUP_POWER)).convert_alpha()
-        healthupImages['superheal'] = pg.image.load(path.join(img_folder, SUPERHEALTHUP_POWER)).convert_alpha()
+        self.healthupImages = {}
+        self.healthupImages['normalheal'] = pg.image.load(path.join(img_folder, HEALTHUP_POWER)).convert_alpha()
+        self.healthupImages['superheal'] = pg.image.load(path.join(img_folder, SUPERHEALTHUP_POWER)).convert_alpha()
         #load score file
         with open(path.join(gameFolder, SCORE_FILE), 'w') as f:
             try:

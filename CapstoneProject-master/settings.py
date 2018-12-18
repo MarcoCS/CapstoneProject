2d6b4f49e9e558277c0e0c5e60e23b050f5a2918
MarcoCS <37008802+Infinity315@users.noreply.github.com>
2018-12-18 11:10:55 -0600

    Revert "Deleted duplicate folder"
    This reverts commit 67046bc327731fefe4714e4ebaee30428b0066aa.

---
+++ CapstoneProject-master/settings.py
@@ -0,0 +1,63 @@
+
+#################################################
+#CS30 Final Capstone Project
+#Developers: Kale, Dallas, Marco
+#
+#Settings
+#################################################
+import pygame as pg
+vec = pg.math.Vector2
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+#Game Settings
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+TITLE = "Top-Down Shooter"
+BGCOLOR = DARKGREY
+
+TILESIZE = 64
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE 
+
+#Gun Settings
+BULLET_IMG = 'tile_187.png'
+BULLET_SPEED = 500     # How fast the bullet travels
+BULLET_LIFETIME = 1000 # When the bullet kills itself in "milliseconds"
+BULLET_RATE = 150      # Delay between shots
+BARREL_OFFSET = vec(30, 10) # Don't change this, this controls where the bullet spawn relative to player
+KICKBACK = 200         # Recoil; how much shooting sends the player back.  This may be changed
+GUN_SPREAD = 8         # How much the bullets deviates
+BULLET_DAMAGE = 10     # Self explanatory
+PELLETS = 1
+
+# Player settings
+PLAYER_HEALTH = 100
+PLAYER_SPEED = 300.0
+PLAYER_ROT_SPEED = 5
+PLAYER_IMG = 'soldier1_gun.png'
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+
+# Mob settings
+MOB_IMG = 'zombie1_hold.png'
+MOB_SPEEDS = [100, 125, 150]
+MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
+AVOID_RADIUS = 50
+MOB_HEALTH = 100
+MOB_DAMAGE = 10
+MOB_KNOCKBACK = 20
+MOB_SCORE = 10
+
+SHOOTER_IMG = 'robot1_gun.png'
+SHOOTER_ROT_SPEED = 5
+SHOOTER_HEALTH = 200
+SHOOTER_SCORE = 15
+

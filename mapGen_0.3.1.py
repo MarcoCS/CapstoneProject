1afba6d0d288b2fb901c390cc2ba7f2a2a54e6e6
MarcoCS <43823775+MarcoCS@users.noreply.github.com>
2019-01-17 13:59:22 -0600

    Updating map gen

--- mapGen_0.3.1.py
+++ mapGen_0.3.1.py
@@ -29,55 +29,150 @@ def print_lst(lst): # Prints the list
 
 # Templates of rooms:
 base_room = [['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],  # This cuts down render time
-            ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
-            ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
-            ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
-            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-            ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-            ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
-            ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
-            ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
-            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
-            ]
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '.', '.', '1', '1', '1', '1']]
 
-empty_room = [[".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."],
-                     [".",".",".",".",".",".",".",".",".","."]
-                    ]
+base_room_top = [['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '.', '.', '1', '1', '1', '1']
+                ]
 
+base_room_bottom = [['1', '1', '1', '1', '.', '.', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+                ]
+
+base_room_top_right = [['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+                ]
+
+base_room_top_left = [['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '.', '.', '1', '1', '1', '1']
+                ]
+
+base_room_bottom_left = [['1', '1', '1', '1', '.', '.', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+                ]
+
+base_room_bottom_right = [['1', '1', '1', '1', '.', '.', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+                ]
+
+base_room_bottom = [['1', '1', '1', '1', '.', '.', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+                ]
+
+base_room_right = [['1', '1', '1', '1', '.', '.', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['.', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '.', '.', '1', '1', '1', '1']
+                ]
+
+base_room_left = [['1', '1', '1', '1', '.', '.', '1', '1', '1', '1'],  # This cuts down render time
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # Before, a function was located in "Rooms_tools" which carved out an area
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],   # We do this anyways, so it simply saves compute power to have a pre-made map.
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '.', '.', '.', '.', '.', '.', '.', '.', '1'],
+                ['1', '1', '1', '1', '.', '.', '1', '1', '1', '1']
+                ]
 # --------------------------------------------------------
 class Enemy_room:
-    def __init__(self, player_level):
-        self.room = copy.deepcopy(base_room)
+    def __init__(self, player_level, room):
+        self.room = copy.deepcopy(room)
         generate_walls(self.room)
         amount_of_enemies = player_level * 4
         for num_enemies in range(amount_of_enemies): # Will spawn the corresponding amount of enemies
             x_rand = rand.randint(2,len(self.room[0]) - 2)
             y_rand = rand.randint(2,len(self.room[0]) - 2)
-            self.room[y_rand][x_rand] = "E"
+            self.room[y_rand][x_rand] = "M"
 
 class Spawn_room:
     def __init__(self):
-        self.room = copy.deepcopy(base_room)
+        type_of_room = base_room_bottom_left
+        self.room = copy.deepcopy(base_room_top_left)
         self.room[5][5] = "P" # Due to limitations with how maps are structured (even number), it is not possible to place the player spawn
         self.room[0][5] = "1"             # Exactly in the middle
         self.room[0][4] = "1"  
    
 class Loot_room:
-    def __init__(self):
-        self.room = copy.deepcopy(base_room)
-        self.room[5][5] = "L"
+    def __init__(self, room):
+        self.room = copy.deepcopy(room)
+        self.room[5][5] = "W"
 
 class Boss_room:
     def __init__(self):
-        self.room = copy.deepcopy(base_room)
+        self.room = copy.deepcopy(base_room_bottom_right)
         self.room[5][5] = "B"
         self.room[9][5] = "1"             # Exactly in the middle
         self.room[9][4] = "1"
@@ -120,25 +215,88 @@ omega_map = [] # This will be the final once everything is processed
 for z in range(len(game_map)):                # First layer of list
     for x in range(len(game_map[0])):         # Secondary layer of nested list
         roll = rand.randint(1,6)
+        type_of_room = base_room
         if roll == 1:
-            game_map[z][x] = Loot_room().room
+            if x == 0:
+                type_of_room = base_room_left
+            else:
+                type_of_room = base_room_right
+            
+            if z == 0:
+                if x == 3:
+                    type_of_room = base_room_top_right
+                else:
+                    type_of_room = base_room_top
+            if z == 3:
+                if x == 0:
+                    type_of_room = base_room_bottom_left
+                else:
+                    type_of_room = base_room_bottom    
+            game_map[z][x] = Loot_room(type_of_room).room
         else:
-            game_map[z][x] = Enemy_room(1).room
+            type_of_room = base_room
+            if x == 0:
+                type_of_room = base_room_left
+            if x == 3:
+                type_of_room = base_room_right
+            
+            if z == 0:
+                if x == 3:
+                    type_of_room = base_room_top_right
+                else:
+                    type_of_room = base_room_top
+            if z == 3:
+                if x == 0:
+                    type_of_room = base_room_bottom_left
+                else:
+                    type_of_room = base_room_bottom   
+            game_map[z][x] = Enemy_room(1,type_of_room).room
+
+        
 game_map[0][0] = Spawn_room().room
 game_map[3][3] = Boss_room().room
 
-for z in range(len(game_map)):                # First layer of list
-    for x in range(len(game_map[0])):         # Secondary layer of nested list  # Third layer
-        for line in game_map[z][x]:
-            print(line)
+map_list = []
+map_list1 = []
+map_list2 = []
+map_list3 = []
+# I did this because, I was too incompetent to do this via a forloop
+map_list.append(zip(game_map[0][0],game_map[0][1],game_map[0][2],game_map[0][3]))
+map_list1.append(zip(game_map[1][0],game_map[1][1],game_map[1][2],game_map[1][3]))
+map_list2.append(zip(game_map[2][0],game_map[2][1],game_map[2][2],game_map[2][3]))
+map_list3.append(zip(game_map[3][0],game_map[3][1],game_map[3][2],game_map[3][3]))
 
-for z in range(len(game_map[0][0])): # This will change the the last element accessed game_map[?][?][z]
-    temp = ""
-    for x in range(len(game_map[0])): # This will change game_map[?][x][?]
-        for y in range(len(game_map)):
-            print(game_map[y][x][z])
-            temp += "".join(game_map[y][x][z])
-    map_file.write(temp)
+# Unpacking 
+map_list = zip(*map_list)
+map_list1 = zip(*map_list1)
+map_list2 = zip(*map_list2)
+map_list3 = zip(*map_list3)
+
+
+
+for x in map_list:
+    y = str(x)
+    y = y.translate({ord(i): None for i in "[]()', "})
+    map_file.write(y)
     map_file.write("\n")
+
+for x in map_list1:
+    y = str(x)
+    y = y.translate({ord(i): None for i in "[]()', "})
+    map_file.write(y)
+    map_file.write("\n")
+
+for x in map_list2:
+    y = str(x)
+    y = y.translate({ord(i): None for i in "[]()', "})
+    map_file.write(y)
+    map_file.write("\n")
+
+for x in map_list3:
+    y = str(x)
+    y = y.translate({ord(i): None for i in "[]()', "})
+    map_file.write(y)
+    map_file.write("\n")
+
 map_file.close()
 # Writing the map to the text file

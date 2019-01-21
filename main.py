a9b7c387085184657a036ca3bbcc4cb9101aeb71
DallasSCS30 <43384932+DallasSCS30@users.noreply.github.com>
2019-01-21 11:37:30 -0600

    New game means starting pistol

--- main.py
+++ main.py
@@ -142,6 +142,8 @@ class Game:
         self.ar = pg.sprite.Group()
         # -------
         
+        Weapons.Starting_pistol.change_var()
+        
         # Floor tiles
         for row, tiles in enumerate(self.map.data):
             for col, tile, in enumerate(tiles):
